add7e235fc52516aaadc7f5ae3b74fabcc977040
rbeer172 <70540400+rbeer172@users.noreply.github.com>
2020-11-20 22:36:20 -0600

    changes

--- WebApp/AirlineWebApp.js
+++ WebApp/AirlineWebApp.js
@@ -1,5 +1,6 @@
-
-var userInfo = []
+var userKey = ["UserName", "UserPhoneNum", "UserEmail", "UserCardNum"];
+var searchKey = ["searchDepartingCity", "searchArrivalCity", "searchStartDate", "SearchendDate", "SearchnumberOfPeople"];
+let tableKey = ['Departing City', 'Arrival City', 'Flight Duration', 'Connecting Flights', 'Fare Condition', 'Price'];
 
 function getInfo()
 {
@@ -8,10 +9,10 @@ function getInfo()
     e.preventDefault();
     for( var i = 0; i < form.elements.length-1; i++)
     {
-      userInfo.push(form.elements[i].value);
+      sessionStorage.setItem(userKey[i], form.elements[i].value);
     }
-    //document.getElementById("output").innerHTML = userInfo[0];
-    window.location.href = 'AirlineWebApp.html';
+    //console.log(sessionStorage.getItem("Departing City"));
+    //window.location.href = 'AirlineWebApp.html';
   });
 }
 
@@ -20,19 +21,23 @@ function searchFlights()
   form = document.getElementById("search");
   form.addEventListener("submit", function(e){
     e.preventDefault();
+    for( var i = 0; i < form.elements.length-1; i++)
+    {
+      sessionStorage.setItem(searchKey[i], form.elements[i].value);
+    }
     window.location.href = 'flightData.html';
   });
 }
 
 function displayResults()
 {
+  var selectedRow = [];
+  let flights = [{ Departing_City: 'Houston', Arrival_City:'Tokyo', Flight_duration:'15 hours', Connecting_Flights: '0', Fare_Condition: 'Economy', Price: '$200'},
+                 { Departing_City: 'Houston', Arrival_City:'Tokyo', Flight_duration:'15 hours', Connecting_Flights: '0', Fare_Condition: 'Economy', Price: '$200'}];
   let myTable = document.getElementById('table');
-  let flights = [{ Departing_City: 'Houston', Arrival_City:'Tokyo', Flight_duration:'15 hours', Connecting_Flights: '0', Fare_Condition: 'Economy', Price: '$200'}]
-  let headers = ['Departing City', 'Arrival City', 'Flight Duration', 'Connecting Flights', 'Fare Condition', 'Price'];
   let table = document.createElement('table');
   let headerRow = document.createElement('tr');
- 
-  headers.forEach(headerText => {
+  tableKey.forEach(headerText => {
     let header = document.createElement('th');
     let textNode = document.createTextNode(headerText);
     header.appendChild(textNode);
@@ -43,7 +48,6 @@ function displayResults()
  
   flights.forEach(emp => {
     let row = document.createElement('tr');
- 
     Object.values(emp).forEach(text => {
       let cell = document.createElement('td');
       let textNode = document.createTextNode(text);
@@ -51,7 +55,25 @@ function displayResults()
       row.appendChild(cell);
       })
     table.appendChild(row);
+
+    row.addEventListener('click', function(){
+      for(var i = 0; i < table.rows.length; i++)
+        table.rows[i].classList.remove('selected');
+      row.classList.add('selected');
+      selectedRow = [];
+      for(var i = 0; i < row.cells.length; i++)
+        selectedRow.push(row.cells[i].innerText);
+    });
   });
  
   myTable.appendChild(table);
+  form = document.getElementById("confirmFlight");
+  form.addEventListener('click', function(e){
+    e.preventDefault();
+    for( var i = 0; i < selectedRow.length; i++)
+    {
+      sessionStorage.setItem(tableKey[i], selectedRow[i]);
+    }
+    window.location.href = 'NewUser.html';
+  });
 }
